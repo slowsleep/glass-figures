afe1079851d01b95b4ff076843c7028091a14d97
slowsleep <yana.alekseeva.it@gmail.com>
2023-12-09 13:17:26 +0300

    Deleted comments, splited logic into functions, created func adding figure to list

--- src/script.js
+++ src/script.js
@@ -5,120 +5,115 @@ const width = document.querySelector("#width");
 const height = document.querySelector("#height");
 const color = document.querySelector("#color");
 const opacity = document.querySelector("#opacity");
+const corner = document.querySelector("#corner");
 
-const cornerCount = document.querySelector(".corner-count");
-cornerCount.style.display = "none";
+const divCorner = document.querySelector(".corner-count");
+divCorner.style.display = "none";
 
 const leftSide = document.querySelector(".left");
 const rightSide = document.querySelector(".right");
 
-
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
-let w = canvas.width = rightSide.offsetWidth * 0.9;
-let h = canvas.height = window.innerHeight * 0.9;
+let w = (canvas.width = rightSide.offsetWidth * 0.9);
+let h = (canvas.height = window.innerHeight * 0.9);
 // canvas.style.backgroundColor = 'transparent';
-canvas.style.backgroundColor = 'white';
+canvas.style.backgroundColor = "white";
+
+let x = 0;
+let y = 0;
+
+let listFigures = [];
 
 
 btnCreate.addEventListener("click", function (e) {
     e.preventDefault();
     let newFigure;
-    if (e.target.value == "polygon") {
-        newFigure = createFigure(selectFigure.value, width.value, height.value, color.value, opacity.value, corner.value);
+    if (selectFigure.value == "polygon") {
+        newFigure = makeFigure(selectFigure.value, x, y, width.value, height.value, color.value, opacity.value, corner.value);
+
     } else {
-        newFigure = createFigure(selectFigure.value, width.value, height.value, color.value, opacity.value);
+        newFigure = makeFigure(selectFigure.value, x, y, width.value, height.value, color.value, opacity.value);
     }
-
-    // const getCoords = (elem) => {
-    //     const box = elem.getBoundingClientRect();
-    //     return {
-    //         top: box.top + scrollY - canvas.offsetTop,
-    //         left: box.left + scrollX - canvas.offsetLeft,
-    //     };
-    // };
-
-    // newFigure.ondragstart = () => false;
-
-    // newFigure.addEventListener("mousedown", function (e) {
-    //     const coords = getCoords(newFigure);
-    //     const shiftX = e.pageX - coords.left;
-    //     const shiftY = e.pageY - coords.top;
-
-    //     const moveAt = (e) => {
-    //         newFigure.style.left = e.pageX - shiftX + "px";
-    //         newFigure.style.top = e.pageY - shiftY + "px";
-    //     };
-
-    //     const theEnd = () => {
-    //         document.removeEventListener("mousemove", moveAt);
-    //         document.removeEventListener("mouseup", theEnd);
-    //     };
-
-    //     newFigure.style.position = "absolute";
-    //     moveAt(e);
-
-    //     document.addEventListener("mousemove", moveAt);
-    //     document.addEventListener("mouseup", theEnd);
-    // });
-    // canvas.appendChild(newFigure);
+    drawFigure(newFigure)
 });
-
 
 selectFigure.addEventListener("change", function (e) {
     if (e.target.value == "polygon") {
-        cornerCount.style.display = "flex";
+        divCorner.style.display = "flex";
     } else {
-        cornerCount.style.display = "none";
+        divCorner.style.display = "none";
     }
 });
 
-width.addEventListener("focus", function() {
+width.addEventListener("focus", function () {
     if (selectFigure.value == "square" || selectFigure.value == "circle") {
         height.value = width.value;
-    } 
-})
+    }
+});
 
-height.addEventListener("focus", function() {
+height.addEventListener("focus", function () {
     if (selectFigure.value == "square" || selectFigure.value == "circle") {
         width.value = height.value;
-    } 
-})
+    }
+});
 
-// function createFigure(figure, width, height, color = "white", opacity = 1, corner = null) {
-//     let div = document.createElement("div");
-//     div.style.width = width.toString() + "px";
-//     div.style.height = height.toString() + "px";
-//     div.style.backgroundColor = color;
-//     div.classList += "figure";
-//     return div;
-// }
 
-function getRGB(hex) {
-    const red = parseInt(hex.substring(1, 3), 16);
-    const green = parseInt(hex.substring(3, 5), 16);
-    const blue = parseInt(hex.substring(5, 7), 16);
-
-    const rgb = [red, green, blue];
-    return rgb;
+function getTextRGBA(color, opacity) {
+    const red = parseInt(color.substring(1, 3), 16);
+    const green = parseInt(color.substring(3, 5), 16);
+    const blue = parseInt(color.substring(5, 7), 16);
+    const rgba = [red, green, blue, opacity];
+    return `rgba(${rgba.join(", ")})`;
 }
 
-function createFigure(figure, width, height, color = "white", opacity = 1, corner = null) {
-    let rgb = getRGB(color);
-    rgb.push(opacity);
-    let textRGBA = `rgba(${rgb.join(", ")})`;
-    ctx.fillStyle = textRGBA;
 
-    if (figure == "rectangle" || figure == "square") {
-        ctx.fillRect(10, 10, width, height);
-    } else if (figure == "circle" || figure == "oval") {
-        ctx.arc(width / 2, width / 2, width / 2, 0, 2*Math.PI);
-        ctx.fill();
-    } else if (figure == "polygon") {
-        ctx.beginPath();
-        ctx.moveTo(160,50);
-        ctx.lineTo(15,250);
-        ctx.lineTo(315,250);
-        ctx.fill();
+function drawRect(figure) {
+    ctx.fillRect(10, 10, figure.width, figure.height);
+}
+
+
+function drawCircle(figure) {
+    ctx.arc(figure.width / 2, figure.width / 2, figure.width / 2, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+
+function drawPolygon(figure) {
+    ctx.beginPath();
+    ctx.moveTo(160, 50);
+    ctx.lineTo(15, 250);
+    ctx.lineTo(315, 250);
+    ctx.closePath();
+    ctx.fill();
+}
+
+
+function drawFigure(figure) {
+    ctx.fillStyle = figure.color;
+
+    if (figure.shape == "rectangle" || figure.shape == "square") {
+        drawRect(figure);
+    } else if (figure.shape == "circle" || figure.shape == "oval") {
+        drawCircle(figure);
+    } else if (figure.shape == "polygon") {
+        drawPolygon(figure);
     }
+}
+
+
+function makeFigure(shape, x=0, y=0, width, height, color="#ffffff", opacity=1, corner=false) {
+    let figure = {
+        shape: shape,
+        x: Number(x),
+        y: Number(y),
+        width: Number(width),
+        height: Number(height),
+        right: Number(x) + Number(width),
+        bottom: Number(y) + Number(height),
+        color: getTextRGBA(color, opacity),
+        corner: Number(corner)
+    };
+    listFigures.push(figure);
+    return figure;
 }
